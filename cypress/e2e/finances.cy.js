77d89703aba736726aad88717c2311cd89895cd4
Natália <48138054+nati-br@users.noreply.github.com>
2023-09-18 16:13:07 -0300

    finalização

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,18 +1,30 @@
+beforeEach(()=>{
+    cy.visit("https://dev-finance.netlify.app")
+});
+function criarTransacao(descricao, valor){
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type(descricao)
+    cy.get('#amount').type(valor)
+    cy.get('#date').type('2023-02-15') // yyyy-mm-dd
+    cy.contains('button','Salvar').click()
+}
 
 describe('Transações', () => {
     it('Cadastrar uma entrada', () => {
-        const str = 'Freela'
-        cy.visit("https://dev-finance.netlify.app")
-        cy.contains("Nova Transação").click()
-        //cy.get('#description').type('Freela')
-        cy.get('#description').type(str)
-        cy.get('#description').should('have.value','Freela')
-        expect(str).to.be.equal('Freela');
-        cy.get('#amount').type(250)
-        cy.get('#date').type('2023-02-15') // yyyy-mm-dd
-        cy.contains('button','Salvar').click()
-   
+       
+        criarTransacao('Freela',250)
+        cy.get('tbody tr td.description').should('have.text','Freela')
+    });
     
-    });    
+    it('Cadastrar uma saída', () => {
+       
+        criarTransacao('cinema',-45)
+    });
+
+    it('Excluir transação', () => {
+        criarTransacao("Freela",100)
+        cy.contains('.description','Freela')
+        .parent().find('img').click()
+    })
 });
 
