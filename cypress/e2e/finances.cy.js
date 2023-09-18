be81d19a30116ce3dedcc621daece9c90c2623aa
Natália <48138054+nati-br@users.noreply.github.com>
2023-09-17 23:56:16 -0300

    anotações

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,9 +1,13 @@
+
 describe('Transações', () => {
     it('Cadastrar uma entrada', () => {
+        const str = 'Freela'
         cy.visit("https://dev-finance.netlify.app")
         cy.contains("Nova Transação").click()
-        cy.get('#description').type('Freela')
+        //cy.get('#description').type('Freela')
+        cy.get('#description').type(str)
         cy.get('#description').should('have.value','Freela')
+        expect(str).to.be.equal('Freela');
         cy.get('#amount').type(250)
         cy.get('#date').type('2023-02-15') // yyyy-mm-dd
         cy.contains('button','Salvar').click()
